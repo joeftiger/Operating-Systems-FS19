889b26c07d1b3110cf81f3422853ddec1567c731
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-02-24 21:39:33 +0100

    Add solution to ex. 2.6

--- Serie01/src/error.c
+++ Serie01/src/error.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int *f(int a) {
-	int b = 2 * a; 
-	return &b;
+	int *b;
+	b = malloc(sizeof(int));
+	*b = 2 * a;
+	return b;
 }
 
 
 int main(void) {
-	int *p4, *p8; 
-	p4 = f(4); 
-	p8 = f(8); 
+	int *p4, *p8;
+	p4 = f(4);
+	p8 = f(8);
 	printf("p4: %i / p8: %i\n", *p4, *p8);
+
+	return 0;
 }
