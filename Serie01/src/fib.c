6bc6967b8b7dbf3160a85644f6656959d09b2feb
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-02-24 21:19:48 +0100

    Added fib.c

--- Serie01/src/fib.c
+++ Serie01/src/fib.c
@@ -2,14 +2,22 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int fib(int a) {
-	/* TODO: implementation */
+	if (a < 0) {
+		printf("Illegal negative input for fibonacci input.\n");
+		exit(1);
+	}
+	if (a == 0) return 0;
+	if (a == 1) return 1;
+
+	return fib(a-1) + fib(a-2);
 }
 
 int main(void)
 {
-	/* TODO: implementation */
-
+	int fib_10 = fib(10);
+	printf("fib(10) = %i", fib_10);
 	return 0;
 }
