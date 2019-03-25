784b653cec1ac7274abfddca4ff1ace6dca74d50
joeftiger <j.oeftiger@protonmail.com>
2019-03-25 22:32:27 +0100

    Series 3 solution (with seg fault)

--- Serie03/src/resources.c
+++ Serie03/src/resources.c
@@ -3,15 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <semaphore.h>
 
 #define MAX_RESOURCES 5
 
 
 int available_resources = MAX_RESOURCES;
 int times = 100000;
-pthread_mutex_t mutex;
-sem_t semaphore; //you may change this to sem_t *semaphore; if more convenient
+pthread_mutex_t *mutex;
 
 
 /* decrease available_resources by count resources
@@ -19,11 +17,14 @@ sem_t semaphore; //you may change this to sem_t *semaphore; if more convenient
  * otherwise return -1 */
 int decrease_count(int count) {
 	/* TODO: Adjust to omit race condition */
+	pthread_mutex_lock(mutex);
 	if (available_resources < count) {
+		pthread_mutex_unlock(mutex);
 		return -1;
 	} else {
 		available_resources -= count;
 		printf("Locked %i resources, now available: %i\n" , count , available_resources);
+		pthread_mutex_unlock(mutex);
 		return 0;
 	}
 }
@@ -31,11 +32,14 @@ int decrease_count(int count) {
 /* increase available resources by count */
 int increase_count(int count) {
 	/* TODO: Adjust to omit race condition */
+	pthread_mutex_lock(mutex);
 	if (count + available_resources > 5) {
+		pthread_mutex_unlock(mutex);
 		return -1;
 	} else {
 		available_resources += count;
 		printf("Freed %i resources, now available: %i\n" , count , available_resources);
+		pthread_mutex_unlock(mutex);
 		return 0;
 	}
 }
@@ -55,13 +59,16 @@ void *runTimes(void *null) {
 
 int main(int argc, char *argv[])
 {
-	pthread_t thread1 , thread0;
+	pthread_t thread0 , thread1;
 
 	decrease_count(2);
 
 	/* TODO: Create 2 threads that call runTimes and wait for their completion
 	 * This should generate false final count of resources every now and then
 	 * when run WITHOUT mutex or semaphore. */
+
+	pthread_create(&thread0, NULL, runTimes, NULL);
+	pthread_create(&thread1, NULL, runTimes, NULL);
 
 	printf("Currently available resources (should be 3): %i\n" , available_resources);
 
