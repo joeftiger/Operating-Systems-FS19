81d4e15e6bab8136080e3b82616cf163b5cc04b7
joeftiger <j.oeftiger@protonmail.com>
2019-03-25 21:54:12 +0100

    Series 1 final

--- Serie01/src/linked_list.c
+++ Serie01/src/linked_list.c
@@ -35,17 +35,49 @@ static void xfree(void *ptr)
 
 static struct elem *init_list(size_t len)
 {
-	/* ... */
+	struct elem *head;
+	struct elem *prev;
+
+	head = xmalloc(sizeof(struct elem));
+	head->pos = 0;
+	head->next = NULL;
+
+	// floating reference to previous item
+	prev = head;
+	for (int i = 1; i < len; i++) {
+		struct elem *curr = xmalloc(sizeof(struct elem));
+		curr->pos = i;
+		curr->next = NULL;
+		prev->next = curr;
+		prev = curr;
+	}
+
+	// link last to first
+	prev->next = head;
+
+	return head;
 }
 
 static void clean_list(struct elem *head, size_t len)
 {
-	/* ... */
+	struct elem *curr = head;
+	for (int i = 0; i < len; i++) {
+		struct elem *next = curr->next;
+		xfree(curr);
+		curr = next;
+	}
 }
 
 static void traverse_list(struct elem *head, int times)
 {
-	/* ... */
+	int start = head->pos;
+	struct elem *curr = head;
+
+	for (int i = 0; times > 0; i++) {
+		printf("(i = %i) -> (%i = pos)\n", i, curr->pos);
+		curr = curr->next;
+		if (start == curr->pos) times--;
+	}
 }
 
 int main(void)
@@ -53,7 +85,9 @@ int main(void)
 	struct elem *head = NULL;
 	size_t len = 10;
 
-	/* ... */
+	head = init_list(len);
+	traverse_list(head, 2);
+	clean_list(head, len);
 
 	return 0;
 }
