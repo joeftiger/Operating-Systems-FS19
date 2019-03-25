f367aa703cc91ac8beb6b9ff5680f498fc7275c1
joeftiger <j.oeftiger@protonmail.com>
2019-03-25 22:43:22 +0100

    Series 3 solution

--- Serie03/src/resources.c
+++ Serie03/src/resources.c
@@ -1,45 +1,68 @@
 /* gcc -Wall -pthread ./resources.c -o resources */
+// TODO Rework
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define MAX_RESOURCES 5
 
 
 int available_resources = MAX_RESOURCES;
 int times = 100000;
-pthread_mutex_t *mutex;
+pthread_mutex_t mutex;
+sem_t semaphore; //you may change this to sem_t *semaphore; if more convenient
 
+void semaphore_init_safe() {
+	int res = sem_init(&semaphore, 0, 1);
+	if (res == -1) {
+		perror("Error while creating semaphore");
+		exit(1);
+	}
+}
+
+void semaphore_wait() {
+	int res = sem_wait(&semaphore);
+	if (res == -1) {
+		perror("Error locking semaphore");
+		exit(1);
+	}
+}
+
+void semaphore_continue() {
+	int res = sem_post(&semaphore);
+	if (res == -1) {
+		perror("Error unlocking semaphore");
+		exit(1);
+	}
+
+}
 
 /* decrease available_resources by count resources
  * return 0 if sufficient resources available,
  * otherwise return -1 */
 int decrease_count(int count) {
-	/* TODO: Adjust to omit race condition */
-	pthread_mutex_lock(mutex);
 	if (available_resources < count) {
-		pthread_mutex_unlock(mutex);
 		return -1;
 	} else {
+		semaphore_wait();
 		available_resources -= count;
 		printf("Locked %i resources, now available: %i\n" , count , available_resources);
-		pthread_mutex_unlock(mutex);
+		semaphore_continue();
 		return 0;
 	}
 }
 
 /* increase available resources by count */
 int increase_count(int count) {
-	/* TODO: Adjust to omit race condition */
-	pthread_mutex_lock(mutex);
 	if (count + available_resources > 5) {
-		pthread_mutex_unlock(mutex);
 		return -1;
 	} else {
+		semaphore_wait();
 		available_resources += count;
 		printf("Freed %i resources, now available: %i\n" , count , available_resources);
-		pthread_mutex_unlock(mutex);
+		semaphore_continue();
 		return 0;
 	}
 }
@@ -53,24 +76,24 @@ void *runTimes(void *null) {
 		while (result < 0) {result = increase_count(1);}
 		i += 1;
 	}
-
 	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	pthread_t thread0 , thread1;
-
+	pthread_t thread1 , thread0;
+	semaphore_init_safe();
 	decrease_count(2);
-
-	/* TODO: Create 2 threads that call runTimes and wait for their completion
-	 * This should generate false final count of resources every now and then
-	 * when run WITHOUT mutex or semaphore. */
 
 	pthread_create(&thread0, NULL, runTimes, NULL);
 	pthread_create(&thread1, NULL, runTimes, NULL);
 
+	pthread_join(thread0, NULL);
+	pthread_join(thread1, NULL);
+
 	printf("Currently available resources (should be 3): %i\n" , available_resources);
+
+	sem_destroy(&semaphore);
 
 	return 0;
 }
